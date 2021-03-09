d7eb81617d36262464b9f60e7309c7687a56bcd4
Your Name <allstarsatw@gmail.com>
2021-03-09 16:29:17 +0300

    Созданы запросы findOne и FindAll для модуля Users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './dto/response/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<UserDto[]> {
+    return this.usersService.findAll();
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './models/user.entity';
+import { UserDto } from './dto/response/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<UserDto[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<UserDto> {
+    return this.usersRepository.findOne(id);
+  }
+}
